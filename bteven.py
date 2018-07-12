cd7ae845769ba9c55b01ceaa95eb4e966d1f893a
kmrkeerthi <41140939+kmrkeerthi@users.noreply.github.com>
2018-07-12 15:00:03 +0530

    Create bteven.py

---
+++ bteven.py
@@ -0,0 +1,11 @@
+n=input()
+n=n.split()
+a=int(n[0])
+b=int(n[1])
+li=[]
+for i in range(a+1,b):
+    if(i%2==0):
+        li.append(i)
+for i in range(0,len(li)-1):
+    print(li[i],end=" ")
+print(li[i+1])
