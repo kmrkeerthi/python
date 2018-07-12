8bfe88032db61b91542faf7e69d3f12b0722ba7f
kmrkeerthi <41140939+kmrkeerthi@users.noreply.github.com>
2018-07-12 12:56:19 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,9 @@
+li=['a','e','i','o','u']
+li1=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+char=input()
+if char in li:
+    print("Vowel")
+elif char in li1:
+    print("Consonent")
+else:
+    print("invalid")
