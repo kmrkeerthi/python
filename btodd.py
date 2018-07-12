95a8c6513d146dc36028ce7ef42042c0dd29f4b7
kmrkeerthi <41140939+kmrkeerthi@users.noreply.github.com>
2018-07-12 14:58:08 +0530

    Update btodd.py

--- btodd.py
+++ btodd.py
@@ -2,6 +2,10 @@ n=input()
 n=n.split()
 a=int(n[0])
 b=int(n[1])
+li=[]
 for i in range(a+1,b):
     if(i%2!=0):
-        print(i,end=" ")
+        li.append(i)
+for i in range(0,len(li)-1):
+    print(li[i],end=" ")
+print(li[i+1])
